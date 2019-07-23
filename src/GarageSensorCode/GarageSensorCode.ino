91e80dc9c602c3c301bd36b4915beda44dbcac6e
tritonengineer <53155154+tritonengineer@users.noreply.github.com>
2019-07-23 16:49:18 -0700

    Update GarageSensorCode.ino

--- src/GarageSensorCode/GarageSensorCode.ino
+++ src/GarageSensorCode/GarageSensorCode.ino
@@ -1,9 +1,62 @@
+#define BLUE 3
+#define GREEN 5
+#define RED 6
+
+#include "SR04.h"
+#define TRIG_PIN 10
+#define ECHO_PIN 11
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+long a;
+
+int buzzer = 12;//the pin of the active buzzer
+
 void setup() {
   // put your setup code here, to run once: modified
 
+pinMode(RED, OUTPUT);
+pinMode(GREEN, OUTPUT);
+pinMode(BLUE, OUTPUT);
+digitalWrite(RED, LOW);
+digitalWrite(GREEN, LOW);
+digitalWrite(BLUE, LOW);
+
+Serial.begin(9600);
+delay(1000);
+
+pinMode(buzzer,OUTPUT);
 }
 
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+a=sr04.Distance();
+
+if(a < 10){
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, LOW);
+  Serial.print(a);
+  Serial.println("cm");
+  delay(1000);
+}
+else if(a < 20){
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(RED, LOW);
+  digitalWrite(BLUE, LOW);
+  Serial.print(a);
+  Serial.println("cm");
+  delay(1000);
+}
+else if(a < 40){
+  digitalWrite(BLUE, HIGH);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(RED, LOW);
+  Serial.print(a);
+  Serial.println("cm");
+  delay(1000);
+}
+
 
 }
