d094b40a40d20661018f0a17bac16fd4df5de45e
tritonengineer <53155154+tritonengineer@users.noreply.github.com>
2019-08-31 15:53:57 -0700

    Update GarageSensorCode.ino

--- src/GarageSensorCode/GarageSensorCode.ino
+++ src/GarageSensorCode/GarageSensorCode.ino
@@ -12,7 +12,6 @@
 #define BLUE_ZONE 250
 
 #include "SR04.h"
-#include <LiquidCrystal.h>
 #define TRIG_PIN 10
 #define ECHO_PIN 11
 SR04 sr04 = SR04(ECHO_PIN, TRIG_PIN);
@@ -21,9 +20,6 @@ int previousColor = OFF;
 unsigned long previousColorChangeMillis;
 
 int buzzer = 12;//the pin of the active buzzer
-
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(7, 8, 9, 13, 4, 2);
 
 void setup() {
   // put your setup code here, to run once: modified
@@ -35,13 +31,11 @@ void setup() {
   digitalWrite(GREEN, LOW);
   digitalWrite(BLUE, LOW);
 
-  Serial.begin(9600);
   delay(1000);
 
   pinMode(buzzer, OUTPUT);
 
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
+
 }
 
 void setLightColor(int redVal, int greenVal, int blueVal) {
@@ -84,11 +78,7 @@ void loop() {
    if(a > 400){
     return;
   }
-  lcd.clear();
-  lcd.setCursor(0, 1);
-  lcd.print(a);
-
-
+  
   if (a < RED_ZONE) {
     handleDistanceChange(RED,3);
   }
@@ -102,9 +92,6 @@ void loop() {
     setLightColor(LOW,LOW,LOW);
     previousColor = OFF;
   }
-
-   Serial.print(a);
-   Serial.println("cm");
    delay(1000);
 
 
